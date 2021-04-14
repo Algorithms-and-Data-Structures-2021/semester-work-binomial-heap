ad3de42df04a571d096ca7eb2267c010ba113bd1
Maxim Gadelev <maximgadelev@mail.ru>
2021-04-14 20:00:58 +0300

    "Add parser to main"

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include "data_structure.hpp"
-#include <ctime>
-#include <chrono>
 #include <fstream>
-#include "iostream"
 #include <string>
 #include <algorithm>
 #include <sstream>
 #include "vector"
+#include <string_view>
+#include <fstream>      // ifstream, ofstream
+#include <iostream>     // cout
+#include <sstream>      // stringstream
+#include <string>       // string, getline
+#include <string_view>  // string_view
+#include <random>       // mt19937_64, random_device
+#include <chrono>
 using namespace std;
 vector<int> split(const string& s, char delimiter) {
   vector<int> tokens;
@@ -19,42 +24,28 @@ vector<int> split(const string& s, char delimiter) {
   return tokens;
 }
 int main() {
-  auto *node7 = new itis::Node(7, 16);
-  auto *node8 = new itis::Node(8, 4);
-  //  heap1->createSampleHeap2();
-  auto *heap2 = new itis::BinomialHeap();
-  string path = "C:\\Users\\111\\CLionProjects\\semester-work-binomial-heap123123\\dataset\\data\\insert\\01.csv";
-  ifstream fin;
-  fin.open(path);
-
-  string line;
+  auto* heap1 = new itis::BinomialHeap();
+  string path = string("C:\\Users\\111\\CLionProjects\\semester-work-binomial-heapheap\\dataset\\data\\dataset-generated10k.csv");
+  ifstream input_stream;
+                           input_stream.open(path);
   vector<int> intValues;
-  if (!fin.is_open()) {
-    cout << "Error" << endl;
-
-  } else {
-
-    while (getline(fin, line)) {
-
-      intValues = split(line, ',');
+  string line;
+  if(!input_stream.is_open()){
+  } else{
+    while(getline(input_stream,line)){
+      intValues=split(line,',');
     }
   }
-
-  auto *heap1 = new itis::BinomialHeap();
-  vector<itis::Node> NodeValues;
+  input_stream.close();
   const auto time_point_before = std::chrono::steady_clock::now();
-  for (int i=0; i < intValues.size()-1; i++) {
-    heap1->insert(intValues[i]);
+
+  for (int i = 0; i <intValues.size() ; ++i) {
+heap1->insert(intValues[i]);
   }
-
   const auto time_point_after = std::chrono::steady_clock::now();
-
   const auto time_diff = time_point_after - time_point_before;
   const long time_elapsed_mks = chrono::duration_cast<chrono::microseconds>(time_diff).count();
-
-  cout << "Time elapsed (ns): " << time_elapsed_mks << '\n';
-  fin.close();
-  return 0;
-
+  cout << "Time elapsed (mks): " << time_elapsed_mks << '\n';
 }
+
 
