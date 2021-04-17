062d81a5ccf93a559769a3b1269361b3a14ab011
UnderWaterr_ <EH6-j3t-2R7-qnv>
2021-04-17 20:50:37 +0300

    generate data

--- main.cpp
+++ main.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include "data_structure.hpp"
+#include <random>  // mt19937_64, random_device
+#include <chrono>  // system_clock
 
 using namespace std;
 
 int main() {
+  const auto seed = chrono::system_clock::now().time_since_epoch().count();
+  auto engine = mt19937(seed);  // без seed`а генератор будет выдавать одни и те же значения
 
-//  auto *node7 = new itis::Node(7, 16);
-//  auto *node8 = new itis::Node(8, 4);
+  //  auto *node7 = new itis::Node(7, 16);
+  //  auto *node8 = new itis::Node(8, 4);
 
   auto *heap1 = new itis::BinomialHeap();
   for (int i = 0; i < 100; ++i) {
-    heap1->insert(5);
+    auto dist = uniform_int_distribution(0, 100);  // равновероятное распределение генерируемых чисел
+    heap1->insert(dist(engine));
   }
   heap1->printHeap();
   heap1->~BinomialHeap();
